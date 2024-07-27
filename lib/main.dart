462dd8397d6e0510c79d07adb2aac377e229285d
SeeBehr <sebi.baer@gmx.de>
2024-07-27 17:39:57 +0200

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:trainings_planner/features/home/home_view.dart';
+import 'package:trainings_planner/theme/theme_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: theme,
+      home: const HomeView(),
+    );
+  }
+}
---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(colorScheme: scheme, useMaterial3: true);
+
+ColorScheme scheme = ColorScheme.fromSeed(seedColor: Colors.lightBlue);
